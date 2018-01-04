3cb9b8d6ed6e85a90dcc18d254eb11bbda1f817a
wangchunye <wangchunye@pwrd.com>
2018-01-04 14:52:02 +0800

    lab2 StringAggregator.java test pass

--- src/java/simpledb/StringAggregator.java
+++ src/java/simpledb/StringAggregator.java
@@ -1,11 +1,21 @@
 package simpledb;
 
+import java.util.*;
+
 /**
  * Knows how to compute some aggregate over a set of StringFields.
  */
 public class StringAggregator implements Aggregator {
 
     private static final long serialVersionUID = 1L;
+
+    private int gbfield;
+    private Type gbfieldtype;
+    private int afield;
+    private Op what;
+    private TupleDesc td;
+
+    private Map<Field, Integer> groups = new HashMap<>();
 
     /**
      * Aggregate constructor
@@ -18,6 +28,13 @@ public class StringAggregator implements Aggregator {
 
     public StringAggregator(int gbfield, Type gbfieldtype, int afield, Op what) {
         // some code goes here
+        if(what != Op.COUNT)
+            throw new IllegalArgumentException("only supports COUNT");
+
+        this.gbfield = gbfield;
+        this.gbfieldtype = gbfieldtype;
+        this.afield = afield;
+        this.what = what;
     }
 
     /**
@@ -26,6 +43,19 @@ public class StringAggregator implements Aggregator {
      */
     public void mergeTupleIntoGroup(Tuple tup) {
         // some code goes here
+        Field groupField = tup.getField(gbfield);
+        Integer count = groups.get(groupField);
+        if(count == null) {
+            count = 0;
+            groups.put(groupField, count);
+        }
+
+        count ++;
+        groups.put(groupField, count);
+
+
+        if(td == null)
+            td = tup.getTupleDesc();
     }
 
     /**
@@ -38,7 +68,52 @@ public class StringAggregator implements Aggregator {
      */
     public OpIterator iterator() {
         // some code goes here
-        throw new UnsupportedOperationException("please implement me for lab2");
+        return new OpIterator() {
+
+            private List<Tuple> aggregateTuples = new ArrayList<>();
+            private Iterator<Tuple> it;
+            private TupleDesc aggregateTupleDesc;
+
+            @Override
+            public void open() throws DbException, TransactionAbortedException {
+                aggregateTupleDesc = getTupleDesc();
+                for(Field key : groups.keySet()) {
+                    Tuple aggTup = new Tuple(aggregateTupleDesc);
+                    int count = groups.get(key);
+                    aggTup.setField(0, key);
+                    aggTup.setField(1, new IntField(count));
+                    aggregateTuples.add(aggTup);
+                }
+                it = aggregateTuples.iterator();
+            }
+
+            @Override
+            public boolean hasNext() throws DbException, TransactionAbortedException {
+                return it.hasNext();
+            }
+
+            @Override
+            public Tuple next() throws DbException, TransactionAbortedException, NoSuchElementException {
+                return it.next();
+            }
+
+            @Override
+            public void rewind() throws DbException, TransactionAbortedException {
+                it = aggregateTuples.iterator();
+            }
+
+            @Override
+            public TupleDesc getTupleDesc() {
+                return new TupleDesc(new Type[]{td.getFieldType(gbfield), Type.INT_TYPE});
+            }
+
+            @Override
+            public void close() {
+                it = null;
+                aggregateTupleDesc = null;
+                aggregateTuples.clear();
+            }
+        };
     }
 
 }
